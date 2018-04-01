218c971d40ffb4a59d18934a6f696db52830d477
Unknown <gamingdevilscc@gmail.com>
2018-04-01 16:46:17 -0400

    cpufreq: add impulse governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -519,6 +519,13 @@ extern struct cpufreq_governor cpufreq_gov_sched;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
 extern struct cpufreq_governor cpufreq_gov_zzmoove;
 #define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_zzmoove)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_ACTIVE)
+extern struct cpufreq_governor cpufreq_gov_blu_active;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_blu_active)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
+extern struct cpufreq_governor cpufreq_gov_impulse;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_impulse)
+>>>>>>> cf15da19f05a... cpufreq: add impulse governor
 #endif
 
 /*********************************************************************
